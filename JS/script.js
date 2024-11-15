a871c1396d193a272fa77bbd8cb6380d94b17182
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2024-11-15 22:04:42 +0100

    Se dota de parámetros la class calculator y se define el objeto casioCalculator

--- JS/script.js
+++ JS/script.js
@@ -2,16 +2,21 @@ const calculatorElement = document.getElementById('calculator-object')
 
 class Calculator {
     constructor(name, brand, model) {
-        this.name = 'Calculadora'
-        this.brand = 'Casio'
-        this.model = 'FX-82ms'
+        this.name = name
+        this.brand = brand
+        this.model = model
     }
 
 
     //Aquí mostramos la información de la calculculadora.//
 
     getInfo() {
-        console.log('Name: ${this.name}, Brand: ${this.brand}, Model: ${this.model}');
+        console.log(`Name: ${this.name}, Brand: ${this.brand}, Model: ${this.model}`);
     }
 }
 
+//Se crea el objeto, que debe de seguir los parámetros indicados en el constructor.//
+
+const casioCalculator = new Calculator('Calculadora', 'Casio', 'FX-82MS')
+
+casioCalculator.getInfo();

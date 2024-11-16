6e04075572b805273221abf4ca0c7e21e7cf4d58
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2024-11-16 21:23:57 +0100

    Se dotan de funcionalidad los botones Up & Down, ahora podemos sumar o restar  números en el display gracias a estos dos botones.

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,15 @@
 const calculatorElement = document.getElementById('calculator-object')
 //Método para el botón ON/OFF y la información en el display.//
-const onOffbutton = document.querySelector('[data-value="ONN_OFF"]')
+const onOffbutton = document.querySelector('[data-value="on_off"]')
 //Método para que los botones solo funcionen con a calculadora encendida.//
-const buttons = document.querySelectorAll('[data-value="ONN_OFF"]')
+const buttons = document.querySelectorAll('[data-value="on_Off"]')
 const display = document.getElementById('display')
+//Método para que los botones Up & Down funcionen.//
+const btnUp = document.querySelector('[data-value="Up"]')
+const btnDown = document.querySelector('[data-value="down"]')
+//Método para que los boones left & Right funcionen.//
+const btnRight = document.querySelector('[data-value="right"]')
+const btnLeft = document.querySelector('[data-value="left"]')
 
 class Calculator {
     constructor(name, brand, model) {
@@ -28,20 +34,49 @@ const casioCalculator = new Calculator('Calculadora', 'Casio', 'FX-82MS')
 //Encendido y apagado de la calculadora y la visualización de la información en el display.//
 
 let encendida = false //se define el estado inicial cómo apagado//
-buttons.forEach(button => button.disable = true) //Se define el estado inicial como desactivado//
+buttons.forEach(button => button.disabled = true) //Se define el estado inicial como desactivado//
 
 onOffbutton.addEventListener('click', () => {
     encendida = !encendida;
 
     if (encendida) {
         display.value = casioCalculator.getInfo()
-        buttons.forEach(button => button.disable = false)
+        buttons.forEach(button => button.disabled = false)
     } else {
         display.value = ''
-        buttons.forEach(button => button.disable = true)
+        buttons.forEach(button => button.disabled = true)
     }
 });
 
+//Función Incremento.//
+btnDown.addEventListener('click', () => {
+    if (encendida && display.value !== '') {
+        let currentValue = parseInt(display.value)
+        if (!isNaN(currentValue)) {
+            currentValue++,
+                display.value = currentValue
+        }
+    }
+})
+
+//Función Decremento.// 
+btnUp.addEventListener('click', () => {
+    if (encendida && display.value !== '') {
+        let currentValue = parseInt(display.value)
+        if (!isNaN(currentValue)) {
+            currentValue--,
+                display.value = currentValue
+        }
+    }
+})
+
+//función cursorPosition Right & Left.//
+
+btnRight.addEventListener('click', () => {
+    if (encendido && display.value !== '') {
+
+    }
+})
 
 
 

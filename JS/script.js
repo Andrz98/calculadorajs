427f2b6d5d348430545f02dbf1f2570bcda99e30
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2024-11-16 01:34:16 +0100

    Se deja completamente funcional el boton ONN_OFF. Entre las funcionalidades del mismo, encontramos: Encendido y apagado, Encendido mostrando los parametros de la calculadora y permitiendo que los demás botones funcionen. Apagado de la calculadora desactivando todos los botones y limpiando el display. Además se agrega la font-family 'doto'.

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,9 @@
 const calculatorElement = document.getElementById('calculator-object')
+//Método para el botón ON/OFF y la información en el display.//
+const onOffbutton = document.querySelector('[data-value="ONN_OFF"]')
+//Método para que los botones solo funcionen con a calculadora encendida.//
+const buttons = document.querySelectorAll('[data-value="ONN_OFF"]')
+const display = document.getElementById('display')
 
 class Calculator {
     constructor(name, brand, model) {
@@ -11,12 +16,32 @@ class Calculator {
     //Aquí mostramos la información de la calculculadora.//
 
     getInfo() {
-        console.log(`Name: ${this.name}, Brand: ${this.brand}, Model: ${this.model}`);
+        return `Name: ${this.name}\nBrand: ${this.brand}\nModel: ${this.model}`; //utilizo return para mostrar la información en el display//
     }
 }
 
-//Se crea el objeto, que debe de seguir los parámetros indicados en el constructor.//
+//Se crea la instancia de la calculadora Casio, que debe de seguir los parámetros indicados en el constructor.//
 
 const casioCalculator = new Calculator('Calculadora', 'Casio', 'FX-82MS')
 
-casioCalculator.getInfo();
+//Programación del display de la calculadora.//
+//Encendido y apagado de la calculadora y la visualización de la información en el display.//
+
+let encendida = false //se define el estado inicial cómo apagado//
+buttons.forEach(button => button.disable = true) //Se define el estado inicial como desactivado//
+
+onOffbutton.addEventListener('click', () => {
+    encendida = !encendida;
+
+    if (encendida) {
+        display.value = casioCalculator.getInfo()
+        buttons.forEach(button => button.disable = false)
+    } else {
+        display.value = ''
+        buttons.forEach(button => button.disable = true)
+    }
+});
+
+
+
+
